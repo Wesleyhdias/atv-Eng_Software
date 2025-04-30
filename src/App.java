3378105ee6183254ee80bfacc846421f22983e3c
Pedro7437 <88412684+Pedro7437@users.noreply.github.com>
2025-04-29 21:52:18 -0300

    Atualização para Permitir Input de Usuários

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        int n = 10; // número de elementos na sequência trocar para o que o usuário quiser
-        int a = 1, b = 1; // também colocar para o usuário escolher os números iniciais
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual é o Tamanho da Sequencia de Fibonacci que você quer? ");
+        int n = sc.nextInt();
+
+        System.out.println();
+
+        System.out.println("Agora, escolha os números iniciais, primeiro o numero a: ");
+        int a = sc.nextInt();
+        
+        System.out.println("Agora, o número b: ");
+        int b = sc.nextInt();
 
         System.out.println("Sequência de Fibonacci com 10 números:");
         for (int i = 0; i < n; i++) {
